6d624cf6ec384df298d5b62645abd0d63907143e
marcin.hareza <marcin.hareza@softwareplant.com>
2021-10-12 15:47:02 +0200

    repackage

--- src/com/mhareza/jdd/collections/bad/team/api/TeamRepository.java
+++ src/com/mhareza/jdd/collections/bad/team/api/TeamRepository.java
@@ -2,11 +2,12 @@ package com.mhareza.jdd.collections.bad.team.api;
 
 import com.mhareza.jdd.collections.bad.team.domain.BoardId;
 import com.mhareza.jdd.collections.bad.team.domain.Team;
+import java.util.List;
 import java.util.Set;
 
 public interface TeamRepository {
 
-	Set<Team> findTeams(Set<BoardId> boardIds);
+	List<Team> findTeams(Set<BoardId> boardIds);
 
-	Set<Team> findTeams(BoardId boardId);
+	List<Team> findTeams(BoardId boardId);
 }
